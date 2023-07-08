5f598a73acdcb6d1341d894068283ba7f29c8083
Javad Shariati <jsh1400@yahoo.com>
2023-07-08 09:43:41 +0330

    Fixed: example

--- example/index.tsx
+++ example/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { MyCounter } from 'f'
+import { MyCounter } from '../src/'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
